f05489286a17561ca09c6d7d2c74f6bceca6cfa0
jiangli <liminghaoze@qq.com>
2012-09-06 13:20:10 +0800

    修改

--- app.js
+++ app.js
@@ -26,8 +26,13 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 app.get('/content',function(req,res){
-    res.send({'content':'最简单来说，一个离线网络应用程序就是一个URL的列表——HTML，CSS，JavaScript，图片，或者其他类型的资源。离线网络应用程序的注意指向一个叫做名单文件并用于定位网络服务器上任何文本文件的列表。用于执行HTML5离线以用程序的网络浏览器将从名单文件中读取URL列表，下载这些资源，将他们在本地缓存，并自动在这些本地副本改变时保持他们更新。当你尝试在没有网络连接时访问网络应用程序，你的网络浏览器将自动切换并使用本地代替。'});
+    res.send({'content':'或者其他类型的资源。你的网络浏览器将自动切换并使用本地代替。'});
 });
+
+//app.get("/offline.manifest", function(req, res){
+//    res.header("Content-Type", "text/cache-manifest");
+//    res.end("CACHE MANIFEST");
+//});
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
